692e6eb5e5fc68e9d6a60591a17464e8fa981bf4
Giulio <giuliovolletti@hotmail.com>
2020-11-23 16:15:27 +0100

    commit 2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 console.log("hello world");
 
 var hamburgerButton = $(".hamburger-menu");
-var iconHamburger = $("i.fa-bars")
-var iconX = $("i.fa-times");
-
-// console.log("icona", iconHamburger);
-// console.log("icona", iconX);
 // console.log(hamburgerButton);
-iconHamburger.click(
-  function (){
-      hamburgerButton.fadeIn(1000);
-  }
-);
-iconX.click(
+var ancorIcon = $("a")
+console.log(ancorIcon);
+
+var display = false;
+ancorIcon.click(
   function(){
+  if ( display == false){
+  hamburgerButton.fadeIn(1000);
+    display = true;
+
+  } else  {
     hamburgerButton.fadeOut(1000);
+    display = false;
+
   }
+}
 )
+
+
+// -------alternativa------- //
+
+// var iconHamburger = $("i.fa-bars")
+// var iconX = $("i.fa-times");
+// // console.log("icona", iconHamburger);
+// // console.log("icona", iconX);
+//
+// iconHamburger.click(
+//   function (){
+//       hamburgerButton.fadeIn(1000);
+//   }
+// );
+// iconX.click(
+//   function(){
+//     hamburgerButton.fadeOut(1000);
+//   }
+// )
